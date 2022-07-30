e6008b24ecb565ebfaedec8237f49ab31d47f253
4812571 <57853417+4812571@users.noreply.github.com>
2022-07-30 16:15:02 -0400

    Knit Services + Controllers

--- src/ServerScriptService/Main.server.lua
+++ src/ServerScriptService/Main.server.lua
@@ -1,5 +1,7 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
 
 local Knit = require(ReplicatedStorage.Packages.Knit)
+Knit.AddServices(ServerScriptService.Services)
 
 Knit.Start()
