0a5e6ee3e6ce4a51c9ee8536780d76068287d71f
Hexcede <8394472+Hexcede@users.noreply.github.com>
2022-07-30 17:49:29 -0400

    Convert indentation to tabs

--- src/ReplicatedStorage/Client/Controllers/BinderController.lua
+++ src/ReplicatedStorage/Client/Controllers/BinderController.lua
@@ -4,31 +4,31 @@ local Knit = require(ReplicatedStorage.Packages.Knit)
 local BinderProvider = require(ReplicatedStorage.Common.BinderProvider)
 
 local BinderController = Knit.CreateController {
-    Name = "BinderController";
+	Name = "BinderController";
 }
 
 BinderController._binderProvider = BinderProvider.new()
 
 function BinderController:Get(tagName)
-    return self._binderProvider:Get(tagName)
+	return self._binderProvider:Get(tagName)
 end
 
 function BinderController:KnitInit()
-    for _, instanceComponentModule in ipairs(ReplicatedStorage.Client.InstanceComponents.Structures) do
-        self._binderProvider:Add(instanceComponentModule.Name, require(instanceComponentModule))
-    end
+	for _, instanceComponentModule in ipairs(ReplicatedStorage.Client.InstanceComponents.Structures) do
+		self._binderProvider:Add(instanceComponentModule.Name, require(instanceComponentModule))
+	end
 
-    for _, instanceComponentModule in ipairs(ReplicatedStorage.Client.InstanceComponents.Tools) do
-        self._binderProvider:Add(instanceComponentModule.Name, require(instanceComponentModule))
-    end
+	for _, instanceComponentModule in ipairs(ReplicatedStorage.Client.InstanceComponents.Tools) do
+		self._binderProvider:Add(instanceComponentModule.Name, require(instanceComponentModule))
+	end
 
-    for _, instanceComponentModule in ipairs(ReplicatedStorage.Client.InstanceComponents.Misc) do
-        self._binderProvider:Add(instanceComponentModule.Name, require(instanceComponentModule))
-    end
+	for _, instanceComponentModule in ipairs(ReplicatedStorage.Client.InstanceComponents.Misc) do
+		self._binderProvider:Add(instanceComponentModule.Name, require(instanceComponentModule))
+	end
 end
 
 function BinderController:KnitStart()
-    self._binderProvider:Start()
+	self._binderProvider:Start()
 end
 
 return BinderController
