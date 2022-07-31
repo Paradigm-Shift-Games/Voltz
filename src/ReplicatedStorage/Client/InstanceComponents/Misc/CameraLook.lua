037400bc1e953d5e9c56bb73daf1bb4c09b86b4e
Hexcede <8394472+Hexcede@users.noreply.github.com>
2022-07-31 19:54:45 -0400

    Improve angle clamping (use Vector3s)

--- src/ReplicatedStorage/Client/InstanceComponents/Misc/CameraLook.lua
+++ src/ReplicatedStorage/Client/InstanceComponents/Misc/CameraLook.lua
@@ -22,6 +22,14 @@ local function dampenAngle(sourceAngle: number, goalAngle: number, angularSpeed:
 	return sourceAngle + math.sign(angleDiff) * TweenService:GetValue(math.clamp(math.min(angularSpeed, math.abs(angleDiff/deltaTime) / angularSpeed), 0, 1), easingStyle, easingDirection) * angularSpeed * deltaTime
 end
 
+local function clampedAngles(angles: Vector3, pitchClamp: Vector3, yawClamp: Vector3, rollClamp: Vector3)
+	return CFrame.Angles(
+		math.clamp(angles.X, pitchClamp.X, pitchClamp.Y),
+		math.clamp(angles.Y, yawClamp.X, yawClamp.Y),
+		math.clamp(angles.Z, rollClamp.X, rollClamp.Y)
+	)
+end
+
 function CameraLook.new(instance: Instance)
 	local self = setmetatable({}, CameraLook)
 
@@ -47,14 +55,23 @@ function CameraLook.new(instance: Instance)
 	-- Speed for rotating the root
 	self.RootPitchSpeed = math.rad(80)
 	self.RootYawSpeed = math.rad(0)
+	self.RootPitchBounds = Vector3.new(-55, 20) * math.rad(1)
+	self.RootYawBounds = Vector3.new(-30, 30) * math.rad(1)
+	self.RootRollBounds = Vector3.new()
 
 	-- Speed for rotating the waist
 	self.WaistPitchSpeed = math.rad(100)
 	self.WaistYawSpeed = math.rad(360)
+	self.WaistPitchBounds = Vector3.new(-55, 30) * math.rad(1)
+	self.WaistYawBounds = Vector3.new(-35, 35) * math.rad(1)
+	self.WaistRollBounds = Vector3.new()
 
 	-- Speed for rotating the neck
 	self.NeckPitchSpeed = math.rad(200)
 	self.NeckYawSpeed = math.rad(560)
+	self.NeckPitchBounds = Vector3.new(-35, 25) * math.rad(1)
+	self.NeckYawBounds = Vector3.new(-45, 45) * math.rad(1)
+	self.NeckRollBounds = Vector3.new()
 
 	-- Grab base C0 for root part (TODO: Clean this up)
 	local lowerTorso: BasePart = instance:WaitForChild("LowerTorso")
@@ -62,12 +79,9 @@ function CameraLook.new(instance: Instance)
 	local rootC0 = root.C0
 
 	-- Reset joint angles
-	self._rootPitch = 0
-	self._rootYaw = 0
-	self._waistPitch = 0
-	self._waistYaw = 0
-	self._neckPitch = 0
-	self._neckYaw = 0
+	self._rootAngles = Vector3.new()
+	self._waistAngles = Vector3.new()
+	self._neckAngles = Vector3.new()
 
 	local player: Player? = Players:GetPlayerFromCharacter(instance)
 	local isLocalPlayer = player == Players.LocalPlayer
@@ -106,42 +120,38 @@ function CameraLook.new(instance: Instance)
 		local lowerTorso: BasePart? = instance:FindFirstChild("LowerTorso")
 		local root: Motor6D? = lowerTorso and lowerTorso:FindFirstChild("Root")
 		if root then
-			self._rootPitch = dampenAngle(self._rootPitch, calculateJointAngle(-upVector, lowerTorso.Position, lookPosition), self.RootPitchSpeed, deltaTime)
-			self._rootYaw = dampenAngle(self._rootYaw, calculateJointAngle(rightVector, lowerTorso.Position, lookPosition), self.RootYawSpeed, deltaTime)
-			local rootCFrame = CFrame.Angles(
-				math.clamp(self._rootPitch, math.rad(-55), math.rad(20)),
-				math.clamp(self._rootYaw, math.rad(-30), math.rad(30)),
-				0
+			self._rootAngles = Vector3.new(
+				dampenAngle(self._rootAngles.X, calculateJointAngle(-upVector, lowerTorso.Position, lookPosition), self.RootPitchSpeed, deltaTime),
+				dampenAngle(self._rootAngles.Y, calculateJointAngle(rightVector, lowerTorso.Position, lookPosition), self.RootYawSpeed, deltaTime)
 			)
 
-			root.Transform *= rootCFrame
-			root.C0 = rootC0 * rootCFrame:Inverse()
+			local rootTransform = clampedAngles(self._rootAngles, self.RootPitchBounds, self.RootYawBounds, self.RootRollBounds)
+			root.Transform *= rootTransform
+			root.C0 = rootC0 * rootTransform:Inverse()
 		end
 
 		-- Waist joint angles
 		local upperTorso: BasePart? = instance:FindFirstChild("UpperTorso")
 		local waist: Motor6D? = upperTorso and upperTorso:FindFirstChild("Waist")
 		if waist then
-			self._waistPitch = dampenAngle(self._waistPitch, calculateJointAngle(-upVector, upperTorso.Position, lookPosition), self.WaistPitchSpeed, deltaTime)
-			self._waistYaw = dampenAngle(self._waistYaw, calculateJointAngle(rightVector, upperTorso.Position, lookPosition), self.WaistYawSpeed, deltaTime)
-			waist.Transform *= CFrame.Angles(
-				math.clamp(self._waistPitch, math.rad(-55), math.rad(30)),
-				math.clamp(self._waistYaw, math.rad(-35), math.rad(35)),
-				0
+			self._waistAngles = Vector3.new(
+				dampenAngle(self._waistAngles.X, calculateJointAngle(-upVector, upperTorso.Position, lookPosition), self.WaistPitchSpeed, deltaTime),
+				dampenAngle(self._waistAngles.Y, calculateJointAngle(rightVector, upperTorso.Position, lookPosition), self.WaistYawSpeed, deltaTime)
 			)
+
+			waist.Transform *= clampedAngles(self._waistAngles, self.WaistPitchBounds, self.WaistYawBounds, self.WaistRollBounds)
 		end
 
 		-- Neck joint angles
 		local head: BasePart? = instance:FindFirstChild("Head")
 		local neck: Motor6D? = head and head:FindFirstChild("Neck")
 		if neck then
-			self._neckPitch = dampenAngle(self._neckPitch, calculateJointAngle(-upVector, head.Position, lookPosition), self.NeckPitchSpeed, deltaTime)
-			self._neckYaw = dampenAngle(self._neckYaw, calculateJointAngle(rightVector, head.Position, lookPosition), self.NeckYawSpeed, deltaTime)
-			neck.Transform *= CFrame.Angles(
-				math.clamp(self._neckPitch, math.rad(-35), math.rad(25)),
-				math.clamp(self._neckYaw, math.rad(-45), math.rad(45)),
-				0
+			self._neckAngles = Vector3.new(
+				dampenAngle(self._neckAngles.X, calculateJointAngle(-upVector, head.Position, lookPosition), self.NeckPitchSpeed, deltaTime),
+				dampenAngle(self._neckAngles.Y, calculateJointAngle(rightVector, head.Position, lookPosition), self.NeckYawSpeed, deltaTime)
 			)
+
+			neck.Transform *= clampedAngles(self._neckAngles, self.NeckPitchBounds, self.NeckYawBounds, self.NeckRollBounds)
 		end
 
 		if isLocalPlayer then
