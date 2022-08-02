dc93a7127f27d2987abaa5e0c0ee375b4c93017f
planetme <87911459+planetme@users.noreply.github.com>
2022-08-02 01:20:15 -0500

    Shifted spaces to tabs

--- src/ReplicatedStorage/Client/InstanceComponents/Misc/FallDamage.lua
+++ src/ReplicatedStorage/Client/InstanceComponents/Misc/FallDamage.lua
@@ -6,35 +6,35 @@ local FallDamage = {}
 FallDamage.__index = FallDamage
 
 function FallDamage:_trackCharacterFall(character: Model, humanoid: Humanoid)
-    local initialHeight = nil
-    local fallHeight = nil
+	local initialHeight = nil
+	local fallHeight = nil
 
-    self._trove:Connect(humanoid.FreeFalling, function(active)
-        if active then
-            initialHeight = character.HumanoidRootPart.Position.Y
-        else
-            fallHeight = initialHeight - character.HumanoidRootPart.Position.Y
-            if fallHeight < FallDamageConfig.Threshold then return end
+	self._trove:Connect(humanoid.FreeFalling, function(active)
+		if active then
+			initialHeight = character.HumanoidRootPart.Position.Y
+		else
+			fallHeight = initialHeight - character.HumanoidRootPart.Position.Y
+			if fallHeight < FallDamageConfig.Threshold then return end
 
-            local damage = (fallHeight - FallDamageConfig.Threshold) * FallDamageConfig.Scale
-            humanoid:TakeDamage(damage)
-        end
-    end)
+			local damage = (fallHeight - FallDamageConfig.Threshold) * FallDamageConfig.Scale
+			humanoid:TakeDamage(damage)
+		end
+	end)
 end
 
 function FallDamage.new(character: Model)
-    local humanoid = character:WaitForChild("Humanoid")
+	local humanoid = character:WaitForChild("Humanoid")
 
-    local self = setmetatable({}, FallDamage)
+	local self = setmetatable({}, FallDamage)
 
-    self._trove = Trove.new()
-    self:_trackCharacterFall(character, humanoid)
+	self._trove = Trove.new()
+	self:_trackCharacterFall(character, humanoid)
 
-    return self
+	return self
 end
 
 function FallDamage:Destroy()
-    self._trove:Clean()
+	self._trove:Clean()
 end
 
 return FallDamage
