e7641abb7e5665847e6fe44d8ed4880bfd6c57b2
luke0x22918 <luke0x14@gmail.com>
2022-08-04 14:19:40 +0200

    Added validation checks

--- src/ServerScriptService/Services/BulletHandlerService/init.lua
+++ src/ServerScriptService/Services/BulletHandlerService/init.lua
@@ -4,11 +4,73 @@ local CollectionService = game:GetService("CollectionService")
 local Players = game:GetService("Players")
 
 local Knit = require(ReplicatedStorage.Packages.Knit)
+local BinderService 
+
+local GunDataTypes = require(ReplicatedStorage.Common.Types.GunDataTypes)
 
 local BulletHandlerService = Knit.CreateService {
     Name = "BulletHandlerService";
     Client = {};
 }
+BulletHandlerService.DEBUG_MODE = true
+
+function BulletHandlerService:_log(str: string, ...)
+    if not self.DEBUG_MODE then
+        return
+    end
+    print("[BulletHandlerService] " .. str:format(...))
+end
+
+local validationTests = {}
+do
+    local barrelMaxCheckDistance = 16
+
+    validationTests["BarrelDistanceCheck"] = function(player: Player, bulletData: GunDataTypes.BulletData, gunConfig: GunDataTypes.GunConfig)
+        local barrelDistance = (bulletData.BarrelPosition - player.Character.RightHand.Position).Magnitude
+        return barrelDistance < barrelMaxCheckDistance
+    end
+
+    local marginOfErrorDistanceIncrement = 0.5
+
+    validationTests["EndPositionCheck"] = function(player: Player, bulletData: GunDataTypes.BulletData, gunConfig: GunDataTypes.GunConfig)
+        local barrelDistance = (bulletData.BarrelPosition - bulletData.EndPosition).Magnitude
+        return barrelDistance < (gunConfig.Range+marginOfErrorDistanceIncrement)
+    end
+
+    validationTests["FireRateCheck"] = function(player: Player, bulletData: GunDataTypes.BulletData, gunConfig: GunDataTypes.GunConfig)
+
+    end
+end
+
+function BulletHandlerService.Client:FireBullet(player: Player, bulletDataList: Array<GunDataTypes.BulletData>)
+    local function handleBulletData(bulletData: GunDataTypes.BulletData)
+        local tool: Tool? = player.Character:FindFirstChildOfClass("Tool")
+        if not tool or tool.Name ~= bulletData.ToolName then
+            return false
+        end
+        local gunConfigName = tool:GetAttribute("ConfigName")
+        local gunConfig = require(ReplicatedStorage.Common.Config.Guns[gunConfigName])
+
+        for checkName, validationTest in validationTests do
+            local success, returnData = pcall(validationTest, player, bulletData, gunConfig)
+            if not success then
+                BulletHandlerService:_log("Validation check '%s' failed due to error: %s", checkName, returnData)
+                return false
+            end
+
+            if returnData == false then
+                BulletHandlerService:_log("Validation check '%s' failed.", checkName)
+                return false
+            end
+        end
+    end
+
+    for _, bulletData in bulletDataList do
+        handleBulletData(bulletData)
+    end
+
+    return 0
+end
 
 local function TempCodeToGiveGunsRemoveLaterOkayThxBye()
     local InsertService = game:GetService("InsertService")
@@ -20,6 +82,7 @@ local function TempCodeToGiveGunsRemoveLaterOkayThxBye()
             CollectionService:RemoveTag(tool, "AssaultRifle")
             CollectionService:AddTag(tool, tagName)
             tool.Name = tagName
+            tool:SetAttribute("ConfigName", tagName)
             tool.Parent = player.Backpack
         end
 
