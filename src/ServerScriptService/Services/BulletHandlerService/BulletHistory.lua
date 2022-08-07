022e9933db1a8e2a2a8e849930226bc2bda346cc
Luke <luke0x14@gmail.com>
2022-08-07 17:39:09 +0200

    BulletHistory cleanup

--- src/ServerScriptService/Services/BulletHandlerService/BulletHistory.lua
+++ src/ServerScriptService/Services/BulletHandlerService/BulletHistory.lua
@@ -1,20 +1,28 @@
 local BulletHistory = {}
 BulletHistory.__index = BulletHistory
 
+type BulletData = {
+    Timestamp: number,
+    BulletAmount: number
+}
+
 function BulletHistory.new()
 	local self = setmetatable({}, BulletHistory)
     self.History = {}
+    self.MaxBulletAge = 5
 	return self
 end
 
 function BulletHistory:AddBulletPoint(bulletAmount: number)
-    table.insert(self.History, 1, {
+    local bulletData: BulletData = {
         Timestamp = os.clock(),
         BulletAmount = bulletAmount
-    })
+    }
+    table.insert(self.History, 1, bulletData)
+    self:Cleanup()
 end
 
-function BulletHistory:GetBulletsFromTimestampOffset(timeOffset: number)
+function BulletHistory:GetBulletsFromTimestampOffset(timeOffset: number): Array<BulletData>
     local bullets = {}
     local timestamp = os.clock()-timeOffset
 
@@ -28,9 +36,21 @@ function BulletHistory:GetBulletsFromTimestampOffset(timeOffset: number)
     return bullets
 end
 
-function BulletHistory:GetBulletCountFromTimestampOffset(timeOffset: number)
+function BulletHistory:GetBulletCountFromTimestampOffset(timeOffset: number): number
     local bulletCount = #self:GetBulletsFromTimestampOffset(timeOffset)
     return bulletCount
+end
+
+function BulletHistory:Cleanup()
+    local minAge = os.clock()-5
+    for i = #self.History, 1, -1 do
+        local bulletHistory = self.History[i]
+        if minAge > bulletHistory.Timestamp then
+            table.remove(self.History, i)
+        else
+            break
+        end
+    end
 end
 
 return BulletHistory
