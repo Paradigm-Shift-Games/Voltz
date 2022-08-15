c72244d41dfd8bae7ba28b938bad988dd1273ad1
miranda <43304302+alfiebgtbhgh@users.noreply.github.com>
2022-08-15 19:21:43 +0100

    Observer Patch 5

--- src/ServerScriptService/InstanceComponents/Structures/Observer.lua
+++ src/ServerScriptService/InstanceComponents/Structures/Observer.lua
@@ -2,6 +2,8 @@ local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
 
+local Trove = require(ReplicatedStorage.Packages.Trove)
+
 local ObserverConfig = require(ReplicatedStorage.Common.Config.Structures.Observer)
 
 local Observer = {}
@@ -10,20 +12,21 @@ Observer.__index = Observer
 function Observer.new(instance)
 	local self = setmetatable({}, Observer)
 	self.Instance = instance
+	self._trove = Trove.new()
 
-    self._heartbeatConnection = RunService.Heartbeat:Connect(function()
-        for _, player in ipairs(Players:GetPlayers()) do
-            if not player.Character then continue end
-            local distanceBetweenParts = (player.Character.HumanoidRootPart.Position - self.Instance.PrimaryPart.Position).Magnitude
-            self.Instance.PrimaryPart.Color = if distanceBetweenParts < ObserverConfig.Range then Color3.fromRGB(0, 255, 0) else Color3.fromRGB(255, 0, 0)
-        end
-    end)
+	self._trove:Connect(RunService.Heartbeat, function()
+		for _, player in ipairs(Players:GetPlayers()) do
+			if not player.Character then continue end
+			local distanceBetweenParts = (player.Character.HumanoidRootPart.Position - self.Instance.PrimaryPart.Position).Magnitude
+			self.Instance.PrimaryPart.Color = if distanceBetweenParts < ObserverConfig.Range then Color3.fromRGB(0, 255, 0) else Color3.fromRGB(255, 0, 0)
+		end
+	end)
 
 	return self
 end
 
 function Observer:Destroy()
-    self._heartbeatConnection:Disconnect()
+	self._trove:Clean()
 end
 
 return Observer
